621927148cef898c0704ad24fdd149ad093dc53a
sivlvy <vladislavsidenko@ukr.net>
2024-06-19 20:32:22 +0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', '@typescript-eslint', 'react'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^Icon$' }],
+    "@typescript-eslint/no-explicit-any": "off"
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export interface AppProps {}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Food Boutique</h1>
+    </div>
+  );
+}
