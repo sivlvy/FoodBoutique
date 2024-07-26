ef7089f58d4edce9f1146bf7699bb46601bd37f3
sivlvy <vladislavsidenko@ukr.net>
2024-07-26 21:20:26 +0300

    updated

--- src/components/Filters/FilterSelect/FilterSelect.tsx
+++ src/components/Filters/FilterSelect/FilterSelect.tsx
@@ -1,7 +1,42 @@
 // import React from "react";
 
+import { useEffect, useState } from "react";
+import { getCategories } from "../../../api/api-products.ts";
+import Select from "react-select";
+
 export interface FilterSelectProps {}
 
 export default function FilterSelect({}: FilterSelectProps) {
-  return <div>FilterSelect</div>;
+  const [categories, setCategories] = useState<any>([]);
+
+  useEffect(() => {
+    const func = async () => {
+      try {
+        const data = await getCategories();
+        const formattedData = data.map((category: string) => ({
+          value: category,
+          label: category,
+        }));
+        setCategories([
+          ...formattedData,
+          { value: "Show All", label: "Show All" },
+        ]);
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    func();
+  }, []);
+
+  return (
+    <div>
+      <Select
+        className="react-select-container"
+        unstyled
+        classNamePrefix="react-select"
+        options={categories}
+      />
+    </div>
+  );
 }
